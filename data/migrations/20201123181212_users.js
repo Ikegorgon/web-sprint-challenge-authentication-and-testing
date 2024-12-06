8072413cdb94dacadb32282d11aafde0af948b89
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2024-12-05 21:55:39 -0700

    Added Jokes Table Migration

--- data/migrations/20201123181212_users.js
+++ data/migrations/20201123181212_users.js
@@ -3,9 +3,16 @@ exports.up = function (knex) {
     users.increments();
     users.string('username', 255).notNullable().unique();
     users.string('password', 255).notNullable();
+  })
+  .createTable('jokes', jokes => {
+    jokes.increments('joke_id');
+    jokes.string('joke', 512).notNullable().unique();
+    jokes.string('punchline', 512).notNullable();
+    jokes.integer('cringe_factor');
+    jokes.integer('created_by').unsigned().notNullable();
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists('jokes').dropTableIfExists('users');
 };
