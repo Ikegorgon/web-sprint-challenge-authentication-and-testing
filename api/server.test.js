5511aadbd73e5ed07674bf4a594eb72041c58fd4
Isaac Lamb <31518729+Ikegorgon@users.noreply.github.com>
2024-12-06 15:16:59 -0700

    Added Tests for Register Endpoint and Confirmed Functionality

--- api/server.test.js
+++ api/server.test.js
@@ -1,15 +1,50 @@
 // Write your tests here
 const request = require('supertest');
 const server = require('./server');
+const db = require('.././data/dbConfig.js');
+
+beforeEach(async () => {
+  await db.migrate.rollback();
+  await db.migrate.latest();
+})
+afterAll(async () => {
+  await db.destroy();
+})
 
 test('sanity', () => {
-  expect(true).not.toBe(false)
+  expect(true).not.toBe(false);
 })
 
 describe('server.js', () => {
   test('[1] Recieves 404 Page not found for any unbuilt endpoints', async () => {
-    const res = await request(server).get('/')
-    expect(res.status).toBe(404)
-    expect(res.body).toStrictEqual({message: "Error 404: Page not found."})
-  })
-})
+    let res = await request(server).get('/asdf');
+    expect(res.status).toBe(404);
+    expect(res.body).toStrictEqual({message: "Error 404: Page not found."});
+    res = await request(server).get('/www');
+    expect(res.status).toBe(404);
+    expect(res.body).toStrictEqual({message: "Error 404: Page not found."});
+    res = await request(server).get('/bob');
+    expect(res.status).toBe(404);
+    expect(res.body).toStrictEqual({message: "Error 404: Page not found."});
+  });
+  test('[2] Register new user successfully', async () => {
+    const res = await request(server).post('/api/auth/register').send({username: "test", password: "12345"});
+    expect(res.status).toBe(201);
+    expect(res.body.id).toBeTruthy();
+    expect(res.body.username).toBeTruthy();
+    expect(res.body.password).toBeTruthy();
+
+  });
+  test('[3] Registering a new user without a username or password results in an error', async () => {
+    let res = await request(server).post('/api/auth/register').send({username: "", password: "12345"});
+    expect(res.body.message).toBe("username and password required");
+    res = await request(server).post('/api/auth/register').send({username: "test", password: ""});
+    expect(res.body.message).toBe("username and password required");
+  });
+  test('[4] Registering a new user needs a unique username', async () => {
+    let res = await request(server).post('/api/auth/register').send({username: "test", password: "12345"});
+    expect(res.status).toBe(201);
+    res = await request(server).post('/api/auth/register').send({username: "test", password: "12345"});
+    expect(res.body.message).toBe("username taken");
+  });
+});
